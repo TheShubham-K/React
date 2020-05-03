754fa757cf0cc5abb3474e618ae8477dca6f4235
shubham-sys <subham.kumar032@gmail.com>
2020-05-03 16:46:06 +0530

    Configuring React

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render( <
+  React.StrictMode >
+  <
+  App / >
+  <
+  /React.StrictMode>,
   document.getElementById('root')
 );
 
